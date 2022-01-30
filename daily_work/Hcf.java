83424c34164996cdad988b807ace6bcffafa4c2f
hemeshwari <nisha7086.nv@gmail.com>
2022-01-30 11:47:18 +0530

    AND OR or NOT operations

---
+++ daily_work/Hcf.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+
+public class Hcf {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        int n1 = scan.nextInt();
+        int n2 = scan.nextInt();
+
+        while(n1%n2!=0){
+            int reminder = n1%n2;
+
+            n1=n2;
+            n2=reminder;
+        }
+        int hcf = n2;
+        System.out.println(hcf);
+
+        scan.close();
+    }
+}
