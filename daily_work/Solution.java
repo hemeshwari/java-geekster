14744687b776590069986aac785a4fa01ad41012
hemeshwari <nisha7086.nv@gmail.com>
2022-01-30 11:47:18 +0530

    all daily work here...

---
+++ daily_work/Solution.java
@@ -0,0 +1,38 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution {
+	
+    public static void main(String[] args) {
+        int i = 4;
+        double d = 4.0;
+        String s = "HackerRank ";
+		
+        Scanner scan = new Scanner(System.in);
+        String c = scan.nextLine();
+
+        int a = scan.nextInt();
+        float b = scan.nextFloat();
+        
+        System.out.println(i+a);
+        System.out.println(d+b);
+        System.out.println(s+c);
+
+        /* Declare second integer, double, and String variables. */
+
+        /* Read and save an integer, double, and String to your variables.*/
+        // Note: If you have trouble reading the entire String, please go back and review the Tutorial closely.
+        
+        /* Print the sum of both integer variables on a new line. */
+
+        /* Print the sum of the double variables on a new line. */
+		
+        /* Concatenate and print the String variables on a new line; 
+        	the 's' variable above should be printed first. */
+
+        scan.close();
+    }
+}
